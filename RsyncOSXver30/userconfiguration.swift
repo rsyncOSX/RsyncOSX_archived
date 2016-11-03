5b611c954b46cb5a2e7d978e3ccd83cb98498684
Thomas Evensen <thomeven@gmail.com>
2016-11-03 13:52:24 +0100

    Automatic dismiss of some informal views, bumped version 3.5.5, updated some comments

--- RsyncOSXver30/userconfiguration.swift
+++ RsyncOSXver30/userconfiguration.swift
@@ -13,57 +13,54 @@ final class userconfiguration {
     
     weak var rsyncchanged_delegate:RsyncChanged?
     
-    init (configRsyncOSX : [NSDictionary]?) {
-        
-        // Setting any configurations read from config file
-        if let userconfiguration = configRsyncOSX {
-            // Read config dictionary as NSDictonary
-            if (userconfiguration.count > 0) {
-                let dict : NSDictionary = userconfiguration[0]
-                
-                // Another version of rsync
-                if let version3rsync = dict.value(forKey: "version3Rsync") as? Int {
-                    if version3rsync == 1 {
-                        SharingManagerConfiguration.sharedInstance.rsyncVer3 = true
-                    } else {
-                        SharingManagerConfiguration.sharedInstance.rsyncVer3 = false
-                    }
-                }
-                
-                // Detailed logging
-                if let detailedlogging = dict.value(forKey: "detailedlogging") as? Int {
-                    if detailedlogging == 1 {
-                        SharingManagerConfiguration.sharedInstance.detailedlogging = true
-                    } else {
-                        SharingManagerConfiguration.sharedInstance.detailedlogging = false
-                    }
-                }
-                
-                // Optional path for rsync
-                if let rsyncPath = dict.value(forKey: "rsyncPath") as? String {
-                    SharingManagerConfiguration.sharedInstance.rsyncPath = rsyncPath
-                }
-                
-                // Disable Excute/taskbutton before scheduled task is commencing
-                if let minutes = dict.value(forKey: "scheduledTaskdisableExecute") as? Double {
-                    SharingManagerConfiguration.sharedInstance.scheduledTaskdisableExecute = minutes
-                }
-                
-                // Allow double click to execute single tasks
-                if let allowDoubleclick = dict.value(forKey: "allowDoubleclick") as? Int {
-                    if allowDoubleclick == 1 {
-                        SharingManagerConfiguration.sharedInstance.allowDoubleclick = true
-                    } else {
-                        SharingManagerConfiguration.sharedInstance.allowDoubleclick = false
-                    }
-                }
+    private func readUserconfiguration(dict : NSDictionary) {
+        // Another version of rsync
+        if let version3rsync = dict.value(forKey: "version3Rsync") as? Int {
+            if version3rsync == 1 {
+                SharingManagerConfiguration.sharedInstance.rsyncVer3 = true
+            } else {
+                SharingManagerConfiguration.sharedInstance.rsyncVer3 = false
             }
         }
+        // Detailed logging
+        if let detailedlogging = dict.value(forKey: "detailedlogging") as? Int {
+            if detailedlogging == 1 {
+                SharingManagerConfiguration.sharedInstance.detailedlogging = true
+            } else {
+                SharingManagerConfiguration.sharedInstance.detailedlogging = false
+            }
+        }
+        // Optional path for rsync
+        if let rsyncPath = dict.value(forKey: "rsyncPath") as? String {
+            SharingManagerConfiguration.sharedInstance.rsyncPath = rsyncPath
+        }
+        // Disable Excute/taskbutton before scheduled task is commencing
+        if let minutes = dict.value(forKey: "scheduledTaskdisableExecute") as? Double {
+            SharingManagerConfiguration.sharedInstance.scheduledTaskdisableExecute = minutes
+        }
+        // Allow double click to execute single tasks
+        if let allowDoubleclick = dict.value(forKey: "allowDoubleclick") as? Int {
+            if allowDoubleclick == 1 {
+                SharingManagerConfiguration.sharedInstance.allowDoubleclick = true
+            } else {
+                SharingManagerConfiguration.sharedInstance.allowDoubleclick = false
+            }
+        }
+    }
+    
+    init (configRsyncOSX : [NSDictionary]?) {
         
-        if let pvc = SharingManagerConfiguration.sharedInstance.ViewObjectMain as? ViewControllertabMain {
-            self.rsyncchanged_delegate = pvc
-            self.rsyncchanged_delegate?.rsyncchanged()
-            self.rsyncchanged_delegate?.displayAllowDoubleclick()
+        // Setting configurations read from config file
+        if let userconfiguration = configRsyncOSX {
+            if (userconfiguration.count > 0) {
+                self.readUserconfiguration(dict: userconfiguration[0])
+            }
+            // If userconfiguration is read from disk update info in main view
+            if let pvc = SharingManagerConfiguration.sharedInstance.ViewObjectMain as? ViewControllertabMain {
+                self.rsyncchanged_delegate = pvc
+                self.rsyncchanged_delegate?.rsyncchanged()
+                self.rsyncchanged_delegate?.displayAllowDoubleclick()
+            }
         }
     }
 }
