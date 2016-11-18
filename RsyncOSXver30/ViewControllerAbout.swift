619d4b9cc16fda0d7ddaa1622918c76dc149756f
Thomas Evensen <thomeven@gmail.com>
2016-11-18 15:05:28 +0100

    Bumped version number to : version 3.6.5 Added about screen. About screen includes checking for new version, link to changelog and documentation. Old (stock in Xcode) about screen is removed.

---
+++ RsyncOSXver30/ViewControllerAbout.swift
@@ -0,0 +1,86 @@
+//
+//  ViewControllerAbout.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 18/11/2016.
+//  Copyright © 2016 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewControllerAbout : NSViewController {
+    
+    // Dismisser
+    weak var dismiss_delegate:DismissViewController?
+    // RsyncOSX version
+    @IBOutlet weak var version: NSTextField!
+    @IBOutlet weak var downloadbutton: NSButton!
+    @IBOutlet weak var thereisanewversion: NSTextField!
+    
+    // new version
+    private var runningVersion : String?
+    private var urlPlist : String?
+    private var urlNewVersion : String?
+    
+    @IBAction func dismiss(_ sender: NSButton) {
+        self.dismiss_delegate?.dismiss_view(viewcontroller: self)
+    }
+    
+    @IBAction func changelog(_ sender: NSButton) {
+        NSWorkspace.shared().open(URL(string: "https://github.com/rsyncOSX/Documentation/blob/master/Docs/Changelog.md")!)
+    }
+    
+    @IBAction func documentation(_ sender: NSButton) {
+        NSWorkspace.shared().open(URL(string: "https://github.com/rsyncOSX/Documentation")!)
+    }
+    
+    @IBAction func download(_ sender: NSButton) {
+        NSWorkspace.shared().open(URL(string: SharingManagerConfiguration.sharedInstance.URLnewVersion!)!)
+    }
+    
+    @IBAction func newversion(_ sender: NSButton) {
+        if let url = URL(string: self.urlPlist!) {
+            do {
+                let contents = NSDictionary (contentsOf: url)
+                if (self.runningVersion != nil) {
+                    if let url = contents?.object(forKey: self.runningVersion!) {
+                        self.urlNewVersion = url as? String
+                        SharingManagerConfiguration.sharedInstance.URLnewVersion = self.urlNewVersion
+                        self.downloadbutton.isEnabled = true
+                        self.thereisanewversion.isHidden = false
+                    } else {
+                        self.thereisanewversion.stringValue = "Looks like the last release "
+                        self.thereisanewversion.isHidden = false
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.urlPlist = "https://dl.dropboxusercontent.com/u/52503631/versionRsyncOSX.plist?raw=1"
+        if let pvc = self.presenting as? ViewControllertabMain {
+            self.dismiss_delegate = pvc
+        }
+        let infoPlist = Bundle.main.infoDictionary
+        let vernumber = infoPlist?["CFBundleShortVersionString"] as! String
+        self.version.stringValue = "RsyncOSX ver: " + vernumber
+        self.runningVersion = vernumber
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        self.downloadbutton.isEnabled = false
+        self.thereisanewversion.isHidden = true
+    }
+    
+    override func viewDidDisappear() {
+        super.viewDidDisappear()
+        self.downloadbutton.isEnabled = false
+        self.thereisanewversion.isHidden = true
+    }
+    
+    
+}
--- RsyncOSXver30/ViewControllertabMain.swift
+++ RsyncOSXver30/ViewControllertabMain.swift
@@ -171,6 +171,14 @@ class ViewControllertabMain : NSViewController, Information, Abort, Count, Refre
             as! NSViewController
     }()
     
+    // About
+    // self.presentViewControllerAsSheet(self.ViewControllerAbout)
+    lazy var ViewControllerAbout: NSViewController = {
+        return self.storyboard!.instantiateController(withIdentifier: "AboutID")
+            as! NSViewController
+    }()
+    
+    
     // Function for dismissing a presented view
     // - parameter viewcontroller: the viewcontroller to be dismissed
     func dismiss_view(viewcontroller:NSViewController) {
@@ -519,6 +527,11 @@ class ViewControllertabMain : NSViewController, Information, Abort, Count, Refre
         }
         
     }
+    
+    @IBAction func About (_ sender: NSButton) {
+        self.presentViewControllerAsModalWindow(self.ViewControllerAbout)
+    }
+    
 
     // Initial functions viewDidLoad and viewDidAppear
     override func viewDidLoad() {
