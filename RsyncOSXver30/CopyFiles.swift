b405fd58b6a87b1be369ac92f4b19fd648fd5f5a
Thomas Evensen <thomas@Thomass-MacBook-Pro.local>
2017-03-04 09:25:20 +0100

    CopyFiles
    Replaced if with guard

--- RsyncOSXver30/CopyFiles.swift
+++ RsyncOSXver30/CopyFiles.swift
@@ -13,22 +13,22 @@ final class CopyFiles {
     // Index from View
     private var index:Int?
     // stack of Work
-    private var work:[enumscpTasks]?
+    private var work:Array<enumscpTasks>?
     // Setting the configuration element according to index
     private var config:configuration?
     // when files.txt is copied from remote server get the records
-    private var files:[String]?
+    private var files:Array<String>?
     // Arguments and command for Process object
-    private var arguments:[String]?
+    private var arguments:Array<String>?
     private var command:String?
     // The arguments object
     var argumentsObject:scpProcessArguments?
     // Message to calling class do a refresh
     weak var refreshtable_delegate:RefreshtableView?
     // Command real run - for the copy process (by rsync)
-    private var argumentsRsync:[String]?
+    private var argumentsRsync:Array<String>?
     // Command dry-run - for the copy process (by rsync)
-    private var argymentsRsyncDrynRun:[String]?
+    private var argymentsRsyncDrynRun:Array<String>?
     // String to display in view
     private var commandDisplay:String?
     // Start and stop progress view
@@ -40,7 +40,7 @@ final class CopyFiles {
     
     
     // Get output from Rsync
-    func getOutput() -> [String] {
+    func getOutput() -> Array<String> {
         return self.output!.getOutput()
     }
     
@@ -49,7 +49,6 @@ final class CopyFiles {
         if (self.task != nil) {
             self.task!.abortProcess()
         }
-        
     }
     
     // Execute Process (either dryrun or realrun)
@@ -84,42 +83,48 @@ final class CopyFiles {
     // This is the initial work when selecting a row to restore from.
     // The stack is .create and .scpFind
     private func doWork() {
-        if (self.work != nil) {
-            if (self.work!.count > 0) {
-                let work:enumscpTasks = (self.work?.removeFirst())!
-                self.argumentsObject = scpProcessArguments(task: work, config: self.config!, remoteFile: nil, localCatalog: nil, drynrun: nil)
-                self.arguments = self.argumentsObject!.getArgs()
-                self.command = self.argumentsObject!.getCommand()
-                self.task = RsyncProcess(operation: false, tabMain: false, command : self.command)
-                self.output = outputProcess()
-                self.task!.executeProcess(self.arguments!, output: self.output!)
-            } else {
-                // Files.txt are ready to read
-                self.files = self.argumentsObject?.getSearchfile()
-                if let pvc = SharingManagerConfiguration.sharedInstance.CopyObjectMain as? ViewControllerCopyFiles {
-                    self.progress_delegate = pvc
-                    self.refreshtable_delegate = pvc
-                    self.refreshtable_delegate?.refresh()
-                    self.progress_delegate?.stop()
-                }
+        
+        guard (self.work != nil) else {
+            return
+        }
+        
+        if (self.work!.count > 0) {
+            let work:enumscpTasks = (self.work?.removeFirst())!
+            self.argumentsObject = scpProcessArguments(task: work, config: self.config!, remoteFile: nil, localCatalog: nil, drynrun: nil)
+            self.arguments = self.argumentsObject!.getArgs()
+            self.command = self.argumentsObject!.getCommand()
+            self.task = RsyncProcess(operation: false, tabMain: false, command : self.command)
+            self.output = outputProcess()
+            self.task!.executeProcess(self.arguments!, output: self.output!)
+        } else {
+            // Files.txt are ready to read
+            self.files = self.argumentsObject?.getSearchfile()
+            if let pvc = SharingManagerConfiguration.sharedInstance.CopyObjectMain as? ViewControllerCopyFiles {
+                self.progress_delegate = pvc
+                self.refreshtable_delegate = pvc
+                self.refreshtable_delegate?.refresh()
+                self.progress_delegate?.stop()
             }
         }
     }
     
     // Filter function
-    func filter(search:String?) -> [String] {
-        if (search != nil) {
-            if (search!.isEmpty == false) {
-                // Filter data
-                return self.files!.filter({$0.contains(search!)})
-            } else {
-                return self.files!
-            }
-        } else {
+    func filter(search:String?) -> Array<String> {
+        
+        guard search != nil else {
             if (self.files != nil) {
-               return self.files!
+                return self.files!
+            } else {
+              return [""]
             }
-            return [""]
+            
+        }
+        
+        if (search!.isEmpty == false) {
+            // Filter data
+            return self.files!.filter({$0.contains(search!)})
+        } else {
+            return self.files!
         }
     }
     
@@ -129,7 +134,7 @@ final class CopyFiles {
         self.index = index
         self.config = SharingManagerConfiguration.sharedInstance.getConfigurations()[self.index!]
         // Create inital stack of work
-        self.work = [enumscpTasks]()
+        self.work = Array<enumscpTasks>()
         // Append workload in reverse order
         // Work are poped of top of stack
         self.work!.append(.create)
