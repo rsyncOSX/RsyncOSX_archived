e16fdd52c48896723b3500ce63230f07447d7fc8
Thomas Evensen <thomas@Thomass-MacBook-Pro.local>
2017-03-15 20:16:17 +0100

    Start on Help View

---
+++ RsyncOSXver30/ViewControllerHelp.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerHelp.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 15.03.2017.
+//  Copyright © 2017 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewControllerHelp: NSViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+    }
+    
+    
+}
