7c597d60869f9e25597faa93827ce239e97df76f
Thomas Evensen <thomeven@gmail.com>
2017-06-27 19:39:24 +0200

    Fixed struct arguments rsync

---
+++ RsyncOSX/argumentsOneConfiguration.swift
@@ -0,0 +1,31 @@
+//
+//  ArgumentsOneConfiguration.swift
+//
+//  Created by Thomas Evensen on 09/02/16.
+//  Copyright © 2016 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+
+// Struct for to store info for ONE configuration.
+// Struct is storing rsync arguments for real run, dryrun 
+// and a version to show in view of both
+
+struct ArgumentsOneConfiguration {
+    
+    var config:configuration?
+    var arg:Array<String>?
+    var argdryRun:Array<String>?
+    var argDisplay:Array<String>?
+    var argdryRunDisplay:Array<String>?
+    
+    init(config:configuration) {
+        
+        self.config = config
+        self.arg = rsyncProcessArguments().argumentsRsync(config, dryRun: false, forDisplay: false)
+        self.argDisplay = rsyncProcessArguments().argumentsRsync(config, dryRun: false, forDisplay: true)
+        self.argdryRun = rsyncProcessArguments().argumentsRsync(config, dryRun: true, forDisplay: false)
+        self.argdryRunDisplay = rsyncProcessArguments().argumentsRsync(config, dryRun: true, forDisplay: true)
+    }
+}
+
