a8dfd514b1361b234cc61b0f47498f796ea7b2b9
Thomas Evensen <thomeven@gmail.com>
2017-06-29 07:18:57 +0200

    Updates from Master

--- RsyncOSX/argumentsOneConfiguration.swift
+++ RsyncOSX/argumentsOneConfiguration.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // Struct for to store info for ONE configuration.
-// Struct is storing rsync arguments for real run, dryrun
+// Struct is storing rsync arguments for real run, dryrun 
 // and a version to show in view of both
 
 struct ArgumentsOneConfiguration {
@@ -21,7 +21,10 @@ struct ArgumentsOneConfiguration {
     
     init(config:configuration) {
         
+        // The configuration
         self.config = config
+        // All arguments for rsync is computed, two sets. One for dry-run and one for real run.
+        // the parameter forDisplay = true computes arguments to display in view.
         self.arg = rsyncProcessArguments().argumentsRsync(config, dryRun: false, forDisplay: false)
         self.argDisplay = rsyncProcessArguments().argumentsRsync(config, dryRun: false, forDisplay: true)
         self.argdryRun = rsyncProcessArguments().argumentsRsync(config, dryRun: true, forDisplay: false)
