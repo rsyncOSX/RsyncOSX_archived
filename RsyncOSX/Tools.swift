2cc62e3ef84dcdcec45bdd076194b7cf85a74eac
Thomas Evensen <thomeven@gmail.com>
2017-07-27 17:07:17 +0200

    SwiftLint

--- RsyncOSX/Tools.swift
+++ RsyncOSX/Tools.swift
@@ -250,4 +250,61 @@ final class Tools {
         return self.macSerialNumber!
     }
 
+    // Calculate seconds from now to startdate
+    private func seconds (_ startdate: Date, enddate: Date?) -> Double {
+        if enddate == nil {
+            return startdate.timeIntervalSinceNow
+        } else {
+            return enddate!.timeIntervalSince(startdate)
+        }
+    }
+
+    // Calculation of time to a spesific date
+    // Used in view of all tasks
+    // Returns time in minutes
+    func timeDoubleMinutes (_ startdate: Date, enddate: Date?) -> Double {
+        let seconds: Double = self.seconds(startdate, enddate: enddate)
+        let (_, minf) = modf (seconds / 3600)
+        let (min, _) = modf (60 * minf)
+        return min
+    }
+
+    // Calculation of time to a spesific date
+    // Used in view of all tasks
+    // Returns time in seconds
+    func timeDoubleSeconds (_ startdate: Date, enddate: Date?) -> Double {
+        let seconds: Double = self.seconds(startdate, enddate: enddate)
+        return seconds
+    }
+
+    // Returns number of hours between start and stop date
+    func timehourInt(_ startdate: Date, enddate: Date?) -> Int {
+        let seconds: Double = self.seconds(startdate, enddate: enddate)
+        let (hr, _) = modf (seconds / 3600)
+        return Int(hr)
+    }
+
+    // Calculation of time to a spesific date
+    // Used in view of all tasks
+    func timeString (_ startdate: Date, enddate: Date?) -> String {
+        var result: String?
+        let seconds: Double = self.seconds(startdate, enddate: enddate)
+        let (hr, minf) = modf (seconds / 3600)
+        let (min, secf) = modf (60 * minf)
+        // hr, min, 60 * secf
+        if hr == 0 && min == 0 {
+            result = String(format:"%.0f", 60 * secf) + " seconds"
+        } else if hr == 0 && min < 60 {
+            result = String(format:"%.0f", min) + " minutes " + String(format:"%.0f", 60 * secf) + " seconds"
+        } else if hr < 25 {
+            result = String(format:"%.0f", hr) + " hours " + String(format:"%.0f", min) + " minutes"
+        } else {
+            result = String(format:"%.0f", hr/24) + " days"
+        }
+        if secf <= 0 {
+            result = " ... working ... "
+        }
+        return result!
+    }
+
 }
