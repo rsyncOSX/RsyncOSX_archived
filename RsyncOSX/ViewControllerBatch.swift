4153c4d49fda8918b17bc4f413b02db6acfc0ea4
Thomas Evensen <thomeven@gmail.com>
2017-11-10 19:29:55 +0100

    nil bug

--- RsyncOSX/ViewControllerBatch.swift
+++ RsyncOSX/ViewControllerBatch.swift
@@ -124,6 +124,9 @@ extension ViewControllerBatch: NSTableViewDelegate {
 
     // TableView delegates
     func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        guard  self.configurations?.getupdatedbatchQueue() != nil else {
+            return nil
+        }
         let object: NSMutableDictionary = self.configurations!.getupdatedbatchQueue()![row]
         if tableColumn!.identifier.rawValue == "estimatedCellID" || tableColumn!.identifier.rawValue == "completedCellID" {
             return object[tableColumn!.identifier] as? Int!
