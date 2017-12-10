a0334222237146adf2136b2bd722932b1b543b30
Thomas Evensen <thomeven@gmail.com>
2017-12-10 12:45:39 +0100

    working on tableview

---
+++ RsyncOSX/Window.swift
@@ -0,0 +1,14 @@
+//
+//  Window.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 10.12.2017.
+//  Copyright © 2017 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class Window: NSWindowController {
+    
+}
