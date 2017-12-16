3a30503e54695402d9832d59b80629d4e2c77dbe
Thomas Evensen <thomeven@gmail.com>
2017-12-16 16:49:32 +0100

    dateformat

--- RsyncOSX/ViewControllerScheduledBackupInWork.swift
+++ RsyncOSX/ViewControllerScheduledBackupInWork.swift
@@ -37,12 +37,17 @@ class ViewControllerScheduledBackupinWork: NSViewController, SetConfigurations, 
 
     private func setInfo() {
         if let dict: NSDictionary = ViewControllerReference.shared.scheduledTask {
-            self.startDate.stringValue = String(describing: (dict.value(forKey: "start") as? Date)!)
+            let dateformatter = Tools().setDateformat()
+            self.startDate.stringValue = dateformatter.string(from: (dict.value(forKey: "start") as? Date)!)
             self.schedule.stringValue = (dict.value(forKey: "schedule") as? String)!
             let hiddenID = (dict.value(forKey: "hiddenID") as? Int)!
             let index = self.configurations!.getIndex(hiddenID)
             let config: Configuration = self.configurations!.getConfigurations()[index]
-            self.remoteServer.stringValue = config.offsiteServer
+            if config.offsiteServer.isEmpty {
+                self.remoteServer.stringValue = "localhost"
+            } else {
+                self.remoteServer.stringValue = config.offsiteServer
+            }
             self.remoteCatalog.stringValue = config.offsiteCatalog
             self.localCatalog.stringValue = config.localCatalog
         }
