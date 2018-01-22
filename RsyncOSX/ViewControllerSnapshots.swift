b385ea831be6698752b25904eac62a7533f140cf
Thomas Evensen <thomeven@gmail.com>
2018-01-22 18:49:10 +0100

    snapshots

---
+++ RsyncOSX/ViewControllerSnapshots.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerSnapshots.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 22.01.2018.
+//  Copyright © 2018 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
