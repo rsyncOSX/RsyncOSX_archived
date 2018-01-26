e519901d05f0d6ea62ab4155485124108ac30a60
Thomas Evensen <thomeven@gmail.com>
2018-01-26 17:40:06 +0100

    sorting

--- RsyncOSX/SnapshotsLoggData.swift
+++ RsyncOSX/SnapshotsLoggData.swift
@@ -30,17 +30,27 @@ final class SnapshotsLoggData {
 
     private func mergedata() {
         guard self.catalogs != nil else { return }
+        var sorted = self.catalogs?.sorted { (di1, di2) -> Bool in
+            let num1 = Int(di1.dropFirst(2)) ?? 0
+            let num2 = Int(di2.dropFirst(2)) ?? 0
+            if num1 <= num2 {
+                return true
+            } else {
+                return false
+            }
+        }
+        // Remove the top ./ catalog
+        if sorted!.count > 1 { sorted?.remove(at: 0) }
+        self.catalogs = sorted
         for i in 0 ..< self.catalogs!.count {
             let snapshotnum = "(" + self.catalogs![i].dropFirst(2) + ")"
             var filter = self.snapshotsloggdata?.filter({($0.value(forKey: "resultExecuted") as? String ?? "").contains(snapshotnum)})
             if filter!.count == 1 {
                 filter![0].setObject(self.catalogs![i], forKey: "snapshotCatalog" as NSCopying)
             } else {
-                if self.catalogs![i] != "./." {
-                    let dict: NSMutableDictionary = ["snapshotCatalog": self.catalogs![i],
-                                                     "dateExecuted": "no logg"]
-                    self.snapshotsloggdata!.append(dict)
-                }
+                let dict: NSMutableDictionary = ["snapshotCatalog": self.catalogs![i],
+                                                 "dateExecuted": "no logg"]
+                self.snapshotsloggdata!.append(dict)
             }
         }
     }
@@ -48,8 +58,8 @@ final class SnapshotsLoggData {
     private func sortedandexpandedcatalog() {
         guard self.expandedcatalogs != nil else { return }
         var sorted = self.expandedcatalogs?.sorted { (di1, di2) -> Bool in
-            let num1 = Int(di1) ?? -1
-            let num2 = Int(di2) ?? -1
+            let num1 = Int(di1) ?? 0
+            let num2 = Int(di2) ?? 0
             if num1 <= num2 {
                 return true
             } else {
@@ -60,7 +70,7 @@ final class SnapshotsLoggData {
         if sorted!.count > 1 { sorted?.remove(at: 0) }
         self.expandedcatalogs = sorted
         for i in 0 ..< self.expandedcatalogs!.count {
-            let expanded = self.config!.localCatalog + self.expandedcatalogs![i]
+            let expanded = self.config!.offsiteCatalog + self.expandedcatalogs![i]
             self.expandedcatalogs![i] = expanded
         }
     }
