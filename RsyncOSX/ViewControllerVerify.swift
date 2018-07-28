6edb83fc16ab93f384dcbb1171f8a7c4fdfc1798
Thomas Evensen <thomeven@gmail.com>
2018-07-28 09:37:24 +0200

    verify and remote info

--- RsyncOSX/ViewControllerVerify.swift
+++ RsyncOSX/ViewControllerVerify.swift
@@ -15,10 +15,27 @@ class ViewControllerVerify: NSViewController, SetConfigurations, GetIndex {
     @IBOutlet weak var outputtable: NSTableView!
     var outputprocess: OutputProcess?
     var index: Int?
-    var hiddenID: Int?
+    var gotremoteinfo: Bool = false
+    private var numbers: NSMutableDictionary?
+    private var complete: Bool = false
+    
     @IBOutlet weak var working: NSProgressIndicator!
     @IBOutlet weak var verifybutton: NSButton!
     @IBOutlet weak var deletedbutton: NSButton!
+
+    @IBOutlet weak var transferredNumber: NSTextField!
+    @IBOutlet weak var transferredNumberSizebytes: NSTextField!
+    @IBOutlet weak var newfiles: NSTextField!
+    @IBOutlet weak var deletefiles: NSTextField!
+    @IBOutlet weak var totalNumber: NSTextField!
+    @IBOutlet weak var totalDirs: NSTextField!
+    @IBOutlet weak var totalNumberSizebytes: NSTextField!
+    @IBOutlet weak var localtotalNumber: NSTextField!
+    @IBOutlet weak var localtotalDirs: NSTextField!
+    @IBOutlet weak var localtotalNumberSizebytes: NSTextField!
+    @IBOutlet weak var gotit: NSTextField!
+    @IBOutlet weak var datelastbackup: NSTextField!
+    @IBOutlet weak var dayslastbackup: NSTextField!
 
     @IBAction func verify(_ sender: NSButton) {
         if self.index != nil {
@@ -53,11 +70,64 @@ class ViewControllerVerify: NSViewController, SetConfigurations, GetIndex {
     override func viewDidAppear() {
         super.viewDidAppear()
         self.index = self.index(viewcontroller: .vctabmain)
+        if let index = self.index {
+            let datelastbackup = self.configurations?.getConfigurations()[index].dateRun ?? "none"
+            let numberlastbackup = self.configurations?.getConfigurations()[index].dayssincelastbackup ?? "none"
+            self.datelastbackup.stringValue = "Date last backup: " + datelastbackup
+            self.dayslastbackup.stringValue = "Days since last backup: " + numberlastbackup
+            self.estimateremoteinfo(index: index, local: true)
+        }
     }
 
     private func enabledisablebuttons(enable: Bool) {
         self.verifybutton.isEnabled = enable
         self.deletedbutton.isEnabled = enable
+    }
+
+    private func estimateremoteinfo(index: Int, local: Bool) {
+        var arguments: [String]?
+        self.working.startAnimation(nil)
+        self.outputprocess = OutputProcess()
+        if local {
+            arguments = self.configurations!.arguments4rsync(index: index, argtype: .argdryRunlocalcataloginfo)
+        } else {
+            arguments = self.configurations!.arguments4rsync(index: index, argtype: .argdryRun)
+        }
+        let verifytask = VerifyTask(arguments: arguments)
+        verifytask.executeProcess(outputprocess: self.outputprocess)
+    }
+
+    private func setNumbers(outputprocess: OutputProcess?, local: Bool) {
+        globalMainQueue.async(execute: { () -> Void in
+            let infotask = RemoteInfoTask(outputprocess: outputprocess)
+            if local {
+                self.numbers = NSMutableDictionary()
+                self.localtotalNumber.stringValue = infotask.totalNumber!
+                self.localtotalNumberSizebytes.stringValue = infotask.totalNumberSizebytes!
+                self.localtotalDirs.stringValue = infotask.totalDirs!
+                self.numbers?.setValue(self.index!, forKey: "index")
+                self.numbers?.setValue(infotask.totalNumber!, forKey: "localtotalNumber")
+                self.numbers?.setValue(infotask.totalNumberSizebytes!, forKey: "localtotalNumberSizebytes")
+                self.numbers?.setValue(infotask.totalDirs!, forKey: "localtotalDirs")
+            } else {
+                self.transferredNumber.stringValue = infotask.transferredNumber!
+                self.transferredNumberSizebytes.stringValue = infotask.transferredNumberSizebytes!
+                self.totalNumber.stringValue = infotask.totalNumber!
+                self.totalNumberSizebytes.stringValue = infotask.totalNumberSizebytes!
+                self.totalDirs.stringValue = infotask.totalDirs!
+                self.newfiles.stringValue = infotask.newfiles!
+                self.deletefiles.stringValue = infotask.deletefiles!
+                self.numbers?.setValue(infotask.transferredNumber!, forKey: "transferredNumber")
+                self.numbers?.setValue(infotask.transferredNumberSizebytes!, forKey: "transferredNumberSizebytes")
+                self.numbers?.setValue(infotask.totalNumber!, forKey: "totalNumber")
+                self.numbers?.setValue(infotask.totalNumberSizebytes!, forKey: "totalNumberSizebytes")
+                self.numbers?.setValue(infotask.totalDirs!, forKey: "totalDirs")
+                self.numbers?.setValue(infotask.newfiles!, forKey: "newfiles")
+                self.numbers?.setValue(infotask.deletefiles!, forKey: "deletefiles")
+                self.working.stopAnimation(nil)
+                self.gotit.stringValue = "Got it..."
+            }
+        })
     }
 }
 
@@ -85,13 +155,27 @@ extension ViewControllerVerify: NSTableViewDelegate {
 
 extension ViewControllerVerify: UpdateProgress {
     func processTermination() {
-        self.working.stopAnimation(nil)
-        self.enabledisablebuttons(enable: true)
+        // self.working.stopAnimation(nil)
+        // self.enabledisablebuttons(enable: true)
+        if self.complete == false {
+            self.setNumbers(outputprocess: self.outputprocess, local: true)
+        } else {
+            self.setNumbers(outputprocess: self.outputprocess, local: false)
+        }
+        if let index = self.index {
+            if self.complete == false {
+                self.complete = true
+                self.outputprocess = OutputProcess()
+                self.estimateremoteinfo(index: index, local: false)
+            }
+        }
     }
 
     func fileHandler() {
+        /*
         globalMainQueue.async(execute: { () -> Void in
             self.outputtable.reloadData()
         })
+        */
     }
 }
