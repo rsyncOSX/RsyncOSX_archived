2a507d2b5d87ef8cb87ddacead964c58ead09976
Thomas Evensen <thomeven@gmail.com>
2018-09-13 08:41:21 +0200

    logging

--- RsyncOSX/Logging.swift
+++ RsyncOSX/Logging.swift
@@ -10,78 +10,84 @@
 import Foundation
 
 class Logging: Reportfileerror {
-
+    
     var outputprocess: OutputProcess?
     var log: String?
     var filename: String?
     var fileURL: URL?
-
-    private func write() {
-        do {
-            try self.log!.write(to: self.fileURL!, atomically: true, encoding: String.Encoding.utf8)
-        } catch let e {
-            let error = e as NSError
-            self.error(error: error.description, errortype: .writelogfile)
-        }
+    
+    private func writeloggfile() {
+        globalMainQueue.async(execute: { () -> Void in
+            do {
+                try self.log!.write(to: self.fileURL!, atomically: true, encoding: String.Encoding.utf8)
+            } catch let e {
+                let error = e as NSError
+                self.error(error: error.description, errortype: .writelogfile)
+            }
+        })
     }
-
-    private func read() {
+    
+    private func readloggfile() {
         do {
             self.log = try String(contentsOf: self.fileURL!, encoding: String.Encoding.utf8)
         } catch let e {
             let error = e as NSError
             self.error(error: error.description, errortype: .openlogfile)
         }
-
     }
-
-    private func logg() {
+    
+    private func minimumlogging() {
         let currendate = Date()
         let dateformatter = Dateandtime().setDateformat()
         let date = dateformatter.string(from: currendate)
-        if ViewControllerReference.shared.fulllogging {
-            self.read()
-            let tmplogg: String = "\n" + "-------------------------------------------\n" + date + "\n"
-                + "-------------------------------------------\n"
-            if self.log == nil {
-                self.log = tmplogg + self.outputprocess!.getOutput()!.joined(separator: "\n")
-            } else {
-                self.log = self.log! + tmplogg  + self.outputprocess!.getOutput()!.joined(separator: "\n")
-            }
-            self.write()
-        } else if ViewControllerReference.shared.minimumlogging {
-            self.read()
-            var tmplogg = [String]()
-            var startindex = self.outputprocess!.getOutput()!.count - 8
-            if startindex < 0 { startindex = 0 }
-            tmplogg.append("\n")
-            tmplogg.append("-------------------------------------------")
-            tmplogg.append(date)
-            tmplogg.append("-------------------------------------------")
-            tmplogg.append("\n")
-            for i in startindex ..< self.outputprocess!.getOutput()!.count {
-                tmplogg.append(self.outputprocess!.getOutput()![i])
-            }
-            if self.log == nil {
-                self.log = tmplogg.joined(separator: "\n")
-            } else {
-                self.log = self.log! + tmplogg.joined(separator: "\n")
-            }
-            self.write()
+        self.readloggfile()
+        var tmplogg = [String]()
+        var startindex = self.outputprocess!.getOutput()!.count - 8
+        if startindex < 0 { startindex = 0 }
+        tmplogg.append("\n")
+        tmplogg.append("-------------------------------------------")
+        tmplogg.append(date)
+        tmplogg.append("-------------------------------------------")
+        tmplogg.append("\n")
+        for i in startindex ..< self.outputprocess!.getOutput()!.count {
+            tmplogg.append(self.outputprocess!.getOutput()![i])
         }
+        if self.log == nil {
+            self.log = tmplogg.joined(separator: "\n")
+        } else {
+            self.log = self.log! + tmplogg.joined(separator: "\n")
+        }
+        self.writeloggfile()
     }
-
-    private func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    
+    private func fulllogging() {
+        let currendate = Date()
+        let dateformatter = Dateandtime().setDateformat()
+        let date = dateformatter.string(from: currendate)
+        self.readloggfile()
+        let tmplogg: String = "\n" + "-------------------------------------------\n" + date + "\n"
+            + "-------------------------------------------\n"
+        if self.log == nil {
+            self.log = tmplogg + self.outputprocess!.getOutput()!.joined(separator: "\n")
+        } else {
+            self.log = self.log! + tmplogg  + self.outputprocess!.getOutput()!.joined(separator: "\n")
+        }
+        self.writeloggfile()
     }
-
+    
     init(outputprocess: OutputProcess?) {
+        guard ViewControllerReference.shared.fulllogging == true || ViewControllerReference.shared.minimumlogging == true else {
+            return
+        }
         self.outputprocess = outputprocess
         self.filename = ViewControllerReference.shared.logname
         let DocumentDirURL = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         self.fileURL = DocumentDirURL?.appendingPathComponent(self.filename!).appendingPathExtension("txt")
         ViewControllerReference.shared.fileURL = self.fileURL
-        self.logg()
+        if ViewControllerReference.shared.fulllogging {
+            self.fulllogging()
+        }  else {
+            self.minimumlogging()
+        }
     }
 }
