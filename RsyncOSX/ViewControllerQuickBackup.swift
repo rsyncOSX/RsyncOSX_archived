09892794572ff879a4d0ee13439701a9d76d0e7a
Thomas Evensen <thomeven@gmail.com>
2018-10-21 09:14:44 +0200

    redesign quickbackup

--- RsyncOSX/ViewControllerQuickBackup.swift
+++ RsyncOSX/ViewControllerQuickBackup.swift
@@ -23,10 +23,8 @@ class ViewControllerQuickBackup: NSViewController, SetDismisser, AbortTask, Dela
     var indexinitiated: Int = -1
 
     @IBOutlet weak var mainTableView: NSTableView!
-    @IBOutlet weak var executeButton: NSButton!
     @IBOutlet weak var abortbutton: NSButton!
     @IBOutlet weak var search: NSSearchField!
-    @IBOutlet weak var noestimates: NSTextField!
     @IBOutlet weak var completed: NSTextField!
 
     // Either abort or close
@@ -34,14 +32,6 @@ class ViewControllerQuickBackup: NSViewController, SetDismisser, AbortTask, Dela
         self.quickbackup = nil
         self.abort()
         self.dismissview(viewcontroller: self, vcontroller: .vctabmain)
-    }
-
-    // Execute batch
-    @IBAction func execute(_ sender: NSButton) {
-        self.executing = true
-        self.executeButton.isEnabled = false
-        self.quickbackup?.prepareandstartexecutetasks()
-        self.reloadtabledata()
     }
 
     private func loadtasks() {
@@ -56,7 +46,6 @@ class ViewControllerQuickBackup: NSViewController, SetDismisser, AbortTask, Dela
         ViewControllerReference.shared.setvcref(viewcontroller: .vcquickbackup, nsviewcontroller: self)
         self.mainTableView.delegate = self
         self.mainTableView.dataSource = self
-        self.search.delegate = self
         self.loadtasks()
     }
 
@@ -68,55 +57,15 @@ class ViewControllerQuickBackup: NSViewController, SetDismisser, AbortTask, Dela
             })
             return
         }
-        self.executeButton.isEnabled = false
-        if let execute = self.enableexecutebutton() {
-            if execute {
-                self.executing = true
-                self.executeButton.isEnabled = false
-                self.quickbackup?.prepareandstartexecutetasks()
-            }
-        }
-        self.reloadtabledata()
-        _ = self.checkforestimates()
+        self.quickbackup?.prepareandstartexecutetasks()
+        globalMainQueue.async(execute: { () -> Void in
+            self.mainTableView.reloadData()
+        })
     }
 
     override func viewDidDisappear() {
         super.viewDidDisappear()
         self.diddissappear = true
-    }
-
-    func tableViewSelectionDidChange(_ notification: Notification) {
-        guard self.executing == false else { return }
-        let myTableViewFromNotification = (notification.object as? NSTableView)!
-        let column = myTableViewFromNotification.selectedColumn
-        if column == 3 {
-            self.filterby = .localcatalog
-            self.quickbackup?.sortbystrings(sort: .localCatalog)
-        } else if column == 4 {
-            self.filterby = .remotecatalog
-            self.quickbackup?.sortbystrings(sort: .offsiteCatalog)
-        } else if column == 5 {
-            self.filterby = .remoteserver
-            self.quickbackup?.sortbystrings(sort: .offsiteServer)
-        } else if column == 6 {
-            self.filterby = .numberofdays
-            self.quickbackup?.sortbydays()
-        } else {
-            return
-        }
-    }
-
-    private func enableexecutebutton() -> Bool? {
-        let backup = self.quickbackup?.sortedlist!.filter({$0.value(forKey: "selectCellID") as? Int == 1})
-        guard backup != nil else { return nil }
-        guard self.executing == false else { return nil }
-        if backup!.count > 0 {
-            self.executeButton.isEnabled = true
-            return true
-        } else {
-            self.executeButton.isEnabled = false
-            return false
-        }
     }
 
     // Progress bars
@@ -134,17 +83,6 @@ class ViewControllerQuickBackup: NSViewController, SetDismisser, AbortTask, Dela
     private func updateProgressbar(progress: NSProgressIndicator) {
         let value = Double((self.inprogresscountDelegate?.inprogressCount())!)
         progress.doubleValue = value
-    }
-
-    private func checkforestimates() -> Bool {
-        if self.quickbackup?.maxcount != nil && self.quickbackup?.maxcount ?? 0  > 0 {
-            self.noestimates.isHidden = true
-            return true
-        } else {
-            self.noestimates.isHidden = false
-            self.max = nil
-            return false
-        }
     }
 
     private func progressintable() -> String {
@@ -165,50 +103,7 @@ extension ViewControllerQuickBackup: NSTableViewDataSource {
     }
 }
 
-extension ViewControllerQuickBackup: NSTableViewDelegate, Attributedestring {
-    // TableView delegates
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        guard self.quickbackup?.sortedlist != nil else { return nil }
-        guard row < self.quickbackup!.sortedlist!.count else { return nil }
-        let object: NSDictionary = (self.quickbackup?.sortedlist![row])!
-        let hiddenID = object.value(forKey: "hiddenID") as? Int
-        if tableColumn!.identifier.rawValue == "daysID" {
-            if object.value(forKey: "markdays") as? Bool == true {
-                let celltext = object[tableColumn!.identifier] as? String
-                return self.attributedstring(str: celltext!, color: NSColor.red, align: .right)
-            }
-        }
-        if tableColumn!.identifier.rawValue == "selectCellID" {
-            return object[tableColumn!.identifier] as? Int
-        }
-        if tableColumn!.identifier.rawValue == "completeCellID" {
-            if object.value(forKey: "inprogressCellID") as? Bool == true {
-                return #imageLiteral(resourceName: "leftarrow")
-            }
-            if object.value(forKey: "completeCellID") as? Bool == true {
-                return #imageLiteral(resourceName: "complete")
-            }
-        }
-        if tableColumn!.identifier.rawValue == "percentCellID" {
-            if hiddenID == self.quickbackup?.hiddenID {
-                return self.progressintable()
-            } else {
-                return ""
-            }
-        }
-        return object[tableColumn!.identifier] as? String
-    }
-
-    // Toggling selection
-    func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
-        guard  self.quickbackup?.sortedlist != nil else { return }
-        if tableColumn!.identifier.rawValue == "selectCellID" {
-            var select: Int = (self.quickbackup?.sortedlist![row].value(forKey: "selectCellID") as? Int)!
-            if select == 0 { select = 1 } else if select == 1 { select = 0 }
-            self.quickbackup?.sortedlist![row].setValue(select, forKey: "selectCellID")
-        }
-        _ = self.enableexecutebutton()
-    }
+extension ViewControllerQuickBackup: NSTableViewDelegate {
 
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         guard self.quickbackup?.sortedlist != nil else { return nil }
@@ -230,10 +125,6 @@ extension ViewControllerQuickBackup: NSTableViewDelegate, Attributedestring {
                     return nil
                 }
             }
-        } else if cellIdentifier == "selectCellID" {
-            return nil
-        } else if cellIdentifier == "completeCellID" {
-            return nil
         } else {
             if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: self) as? NSTableCellView {
                 cell.textField?.stringValue = object.value(forKey: cellIdentifier) as? String ?? ""
@@ -248,7 +139,6 @@ extension ViewControllerQuickBackup: Reloadandrefresh {
 
     // Updates tableview according to progress of batch
     func reloadtabledata() {
-        _ = self.enableexecutebutton()
         globalMainQueue.async(execute: { () -> Void in
             self.mainTableView.reloadData()
         })
@@ -277,32 +167,6 @@ extension ViewControllerQuickBackup: UpdateProgress {
     func fileHandler() {
         globalMainQueue.async(execute: { () -> Void in
             self.mainTableView.reloadData()
-        })
-    }
-}
-
-extension ViewControllerQuickBackup: NSSearchFieldDelegate {
-
-    override func controlTextDidChange(_ obj: Notification) {
-        guard self.executing == false else { return }
-        self.delayWithSeconds(0.25) {
-            let filterstring = self.search.stringValue
-            if filterstring.isEmpty {
-                globalMainQueue.async(execute: { () -> Void in
-                    self.quickbackup?.sortbydays()
-                })
-            } else {
-                globalMainQueue.async(execute: { () -> Void in
-                    self.quickbackup?.filter(search: filterstring, filterby: self.filterby)
-                })
-            }
-        }
-    }
-
-    func searchFieldDidEndSearching(_ sender: NSSearchField) {
-        guard self.executing == false else { return }
-        globalMainQueue.async(execute: { () -> Void in
-            self.loadtasks()
         })
     }
 }
