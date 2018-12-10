291a608f1e1c7d7892d18ed0d31e8a66dd2ccff9
Thomas Evensen <thomeven@gmail.com>
2018-12-10 13:33:29 +0100

    snapshots

--- RsyncOSX/PlanSnapshots.swift
+++ RsyncOSX/PlanSnapshots.swift
@@ -18,6 +18,26 @@ class PlanSnapshots {
     weak var SnapshotsLoggDataDelegate: GetSnapshotsLoggData?
     var snapshotsloggdata: SnapshotsLoggData?
 
+    // 1 - 7 days
+    private func lasteweek() {
+        
+    }
+    
+    // 8 - 28/29/30/31 days
+    private func lastmonth() {
+        
+    }
+
+    // 31 - 365 days
+    private func lastyear() {
+        
+    }
+
+    // 365 days
+    private func previousyears() {
+        
+    }
+
     init() {
         self.SnapshotsLoggDataDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vcsnapshot) as? ViewControllerSnapshots
         self.snapshotsloggdata = self.SnapshotsLoggDataDelegate?.getsnapshotsloggaata()
