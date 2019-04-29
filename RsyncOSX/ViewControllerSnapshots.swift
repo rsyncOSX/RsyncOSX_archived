0ec18761aad03253eded239ffe7b394059478d11
Thomas Evensen <thomeven@gmail.com>
2019-04-29 19:35:24 +0200

    Localization

--- RsyncOSX/ViewControllerSnapshots.swift
+++ RsyncOSX/ViewControllerSnapshots.swift
@@ -178,8 +178,8 @@ class ViewControllerSnapshots: NSViewController, SetDismisser, SetConfigurations
     @IBAction func delete(_ sender: NSButton) {
         guard self.snapshotsloggdata != nil else { return }
         let question: String = NSLocalizedString("Do you REALLY want to DELETE selected snapshots?", comment: "Snapshots")
-        let text: String = NSLocalizedString("Cancel or Delete?", comment: "Snapshots")
-        let dialog: String = NSLocalizedString("Delete", comment: "Snapshots")
+        let text: String = NSLocalizedString("Cancel or Delete?", comment: "Loggs, Tasks and Profile")
+        let dialog: String = NSLocalizedString("Delete", comment: "Loggs, Tasks and Profile")
         let answer = Alerts.dialogOrCancel(question: question, text: text, dialog: dialog)
         if answer {
             self.info(num: 0)
