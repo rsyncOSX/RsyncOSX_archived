0727e0fcfce3ca2016266872852230bbc791d403
Thomas Evensen <thomeven@gmail.com>
2019-05-01 19:14:48 +0200

    Localization

--- RsyncOSX/ViewControllerVerify.swift
+++ RsyncOSX/ViewControllerVerify.swift
@@ -21,6 +21,8 @@ class ViewControllerVerify: NSViewController, SetConfigurations, Index, VcExecut
     private var numbers: NSMutableDictionary?
     private var complete: Bool = false
     private var processRefererence: ProcessCmd?
+    let lastdate: String = NSLocalizedString("Date last backup:", comment: "Verify")
+    let dayssince: String = NSLocalizedString("Days since last backup:", comment: "Verify")
 
     @IBOutlet weak var working: NSProgressIndicator!
     @IBOutlet weak var verifybutton: NSButton!
@@ -79,8 +81,9 @@ class ViewControllerVerify: NSViewController, SetConfigurations, Index, VcExecut
         self.rsynccommanddisplay.stringValue = Verifyrsyncpath().displayrsynccommand(index: self.index!, display: .verify)
         self.verifyradiobutton.state = .on
         self.changedradiobutton.state = .off
-         self.gotit.textColor = .white
-        self.gotit.stringValue = "Verifying, please wait..."
+        self.gotit.textColor = .white
+        let gotit: String = NSLocalizedString("Verifying, please wait...", comment: "Verify")
+        self.gotit.stringValue = gotit
         self.enabledisablebuttons(enable: false)
         self.working.startAnimation(nil)
         let arguments = self.configurations?.arguments4verify(index: self.index!)
@@ -98,7 +101,8 @@ class ViewControllerVerify: NSViewController, SetConfigurations, Index, VcExecut
         self.changedradiobutton.state = .on
         self.verifyradiobutton.state = .off
         self.gotit.textColor = .white
-        self.gotit.stringValue = "Computing changed, please wait..."
+        let gotit: String = NSLocalizedString("Computing changed, please wait...", comment: "Verify")
+        self.gotit.stringValue = gotit
         self.enabledisablebuttons(enable: false)
         self.working.startAnimation(nil)
         let arguments = self.configurations?.arguments4restore(index: self.index!, argtype: .argdryRun)
@@ -159,10 +163,8 @@ class ViewControllerVerify: NSViewController, SetConfigurations, Index, VcExecut
             self.complete = false
             let datelastbackup = self.configurations?.getConfigurations()[index].dateRun ?? "none"
             let numberlastbackup = self.configurations?.getConfigurations()[index].dayssincelastbackup ?? "none"
-            let lastdate: String = NSLocalizedString("Date last backup: ", comment: "Verify")
-            let dayssince: String = NSLocalizedString("Days since last backup: ", comment: "Verify")
-            self.datelastbackup.stringValue = lastdate + datelastbackup
-            self.dayslastbackup.stringValue = dayssince + numberlastbackup
+            self.datelastbackup.stringValue = self.lastdate + datelastbackup
+            self.dayslastbackup.stringValue = self.dayssince + numberlastbackup
             self.estimateremoteinfo(index: index, local: true)
         } else {
             self.gotit.textColor = .red
@@ -255,10 +257,8 @@ class ViewControllerVerify: NSViewController, SetConfigurations, Index, VcExecut
         self.totalDirs.stringValue = ""
         self.newfiles.stringValue = ""
         self.deletefiles.stringValue = ""
-        let lastdate: String = NSLocalizedString("Date last backup:", comment: "Verify")
-        let dayssince: String = NSLocalizedString("Days since last backup:", comment: "Verify")
-        self.datelastbackup.stringValue = lastdate
-        self.dayslastbackup.stringValue = dayssince
+        self.datelastbackup.stringValue = self.lastdate
+        self.dayslastbackup.stringValue = self.dayssince
         self.rsynccommanddisplay.stringValue = ""
         self.verifyradiobutton.state = .off
         self.changedradiobutton.state = .off
