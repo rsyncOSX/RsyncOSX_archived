bb0f3a3b340320910172590204e020a0a318a1f9
Thomas Evensen <thomeven@gmail.com>
2019-06-28 07:57:11 +0200

    XCTest

--- RsyncOSXTests/RsyncOSXTests.swift
+++ RsyncOSXTests/RsyncOSXTests.swift
@@ -13,6 +13,7 @@ class RsyncOSXTests: XCTestCase, SetConfigurations {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+         _ = Selectprofile(profile: "XCTest")
     }
 
     override func tearDown() {
@@ -20,8 +21,7 @@ class RsyncOSXTests: XCTestCase, SetConfigurations {
     }
 
     func testnumberofargumentstorsync() {
-        _ = Selectprofile(profile: "XCTest")
-        let count = self.configurations?.arguments4rsync(index: 1, argtype: .argdryRun).count
+        let count = self.configurations?.arguments4rsync(index: 0, argtype: .argdryRun).count
         XCTAssertEqual(count, 14, "Should be equal to 14")
     }
 
@@ -29,4 +29,21 @@ class RsyncOSXTests: XCTestCase, SetConfigurations {
         let count = self.configurations?.getConfigurations().count
         XCTAssertEqual(count, 2, "Should be equal to 2")
     }
+
+    func testargumentsdryrun() {
+        let arguments = ["--archive", "--verbose", "--compress", "--delete", "-e", "ssh -p 22", "--exclude=.git",
+                         "--backup", "--backup-dir=../backup_XCTest", "--suffix=_$(date +%Y-%m-%d.%H.%M)", "--dry-run",
+                         "--stats", "/Users/thomas/XCTest/", "thomas@10.0.0.57:/backup2/RsyncOSX/XCTest/"]
+        XCTAssertEqual(arguments, self.configurations?.arguments4rsync(index: 0, argtype: .argdryRun),
+                       "Arguments should be equal")
+    }
+
+    func testargumentsrealrun() {
+        let arguments = ["--archive", "--verbose", "--compress", "--delete", "-e", "ssh -p 22", "--exclude=.git",
+                         "--backup", "--backup-dir=../backup_XCTest", "--suffix=_$(date +%Y-%m-%d.%H.%M)",
+                         "--stats", "/Users/thomas/XCTest/", "thomas@web:~/XCTest/"]
+        XCTAssertEqual(arguments, self.configurations?.arguments4rsync(index: 1, argtype: .arg),
+                       "Arguments should be equal")
+    }
+
 }
