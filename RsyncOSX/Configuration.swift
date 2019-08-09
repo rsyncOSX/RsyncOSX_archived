6be6955988cbd6fd5127c972c155ab3c29a96274
Thomas Evensen <thomeven@gmail.com>
2019-08-09 13:38:48 +0200

    Update Configuration.swift

--- RsyncOSX/Configuration.swift
+++ RsyncOSX/Configuration.swift
@@ -58,7 +58,16 @@ struct Configuration {
         self.localCatalog = dictionary.object(forKey: "localCatalog") as? String ?? ""
         self.offsiteCatalog = dictionary.object(forKey: "offsiteCatalog") as? String ?? ""
         self.offsiteUsername = dictionary.object(forKey: "offsiteUsername") as? String ?? ""
-        self.batch = dictionary.object(forKey: "batch") as? Int ?? 0
+        // Convert string batch to Int
+        if let batch = dictionary.object(forKey: "batch") as? String {
+            if batch == "yes" {
+                self.batch = 1
+            } else {
+                self.batch = 0
+            }
+        } else {
+            self.batch = dictionary.object(forKey: "batch") as? Int ?? 0
+        }
         self.parameter1 = dictionary.object(forKey: "parameter1") as? String ?? ""
         self.parameter2 = dictionary.object(forKey: "parameter2") as? String ?? ""
         self.parameter3 = dictionary.object(forKey: "parameter3") as? String ?? ""
