d0778eb20878b27e74b21caf6321051ae65f59a3
Thomas Evensen <thomeven@gmail.com>
2019-08-16 10:01:44 +0200

    Update

--- RsyncOSX/ViewControllerEstimatingTasks.swift
+++ RsyncOSX/ViewControllerEstimatingTasks.swift
@@ -5,7 +5,7 @@
 //  Created by Thomas Evensen on 21.04.2018.
 //  Copyright © 2018 Thomas Evensen. All rights reserved.
 //
-// swiftlint:disable line_length cyclomatic_complexity
+// swiftlint:disable line_length
 
 import Foundation
 import Cocoa
