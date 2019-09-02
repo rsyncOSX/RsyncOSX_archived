4b0f3b26192095c7036c73bc43bb6f8eea2ebff5
Thomas Evensen <thomeven@gmail.com>
2019-09-02 16:24:56 +0200

    Update ViewControllerExtensions.swift

--- RsyncOSX/ViewControllerExtensions.swift
+++ RsyncOSX/ViewControllerExtensions.swift
@@ -191,7 +191,6 @@ protocol DeselectRowTable: class {
 protocol Deselect {
     var deselectDelegateMain: DeselectRowTable? { get }
     var deselectDelegateSchedule: DeselectRowTable? { get }
-    func deselectrowtable(vcontroller: ViewController)
 }
 
 extension Deselect {
@@ -345,12 +344,11 @@ protocol Sorting {
 
 extension Sorting {
     func sortbydate(notsortedlist: [NSMutableDictionary]?, sortdirection: Bool) -> [NSMutableDictionary]? {
-        guard notsortedlist != nil else { return nil }
         let dateformatter = DateFormatter()
         dateformatter.formatterBehavior = .behavior10_4
         dateformatter.dateStyle = .medium
         dateformatter.timeStyle = .short
-        let sorted = notsortedlist!.sorted { (dict1, dict2) -> Bool in
+        let sorted = notsortedlist?.sorted { (dict1, dict2) -> Bool in
             let date1localized = dateformatter.date(from: (dict1.value(forKey: "dateExecuted") as? String) ?? "")
             if let date2localized = dateformatter.date(from: (dict2.value(forKey: "dateExecuted") as? String) ?? "") {
                 if date1localized?.timeIntervalSince(date2localized) ?? -1 > 0 {
@@ -366,7 +364,6 @@ extension Sorting {
     }
 
     func sortbystring(notsortedlist: [NSMutableDictionary]?, sortby: Sortandfilter, sortdirection: Bool) -> [NSMutableDictionary]? {
-        guard notsortedlist != nil else { return nil }
         let sortstring = self.filterbystring(filterby: sortby)
         let sorted = notsortedlist?.sorted { (dict1, dict2) -> Bool in
             if (dict1.value(forKey: sortstring) as? String) ?? "" > (dict2.value(forKey: sortstring) as? String) ?? "" {
