9cc9b367514270f4053f5bdaa261cb1d04a78264
Thomas Evensen <thomeven@gmail.com>
2020-01-08 18:02:37 +0100

    Update Validatenewconfigs.swift

--- RsyncOSX/Validatenewconfigs.swift
+++ RsyncOSX/Validatenewconfigs.swift
@@ -11,29 +11,6 @@ import Foundation
 
 struct Validatenewconfigs {
     var validated: Bool = false
-    /*
-     let dict: NSMutableDictionary = [
-                "task": config.task,
-                "backupID": config.backupID,
-                "localCatalog": config.localCatalog,
-                "offsiteCatalog": config.offsiteCatalog,
-                "batch": config.batch,
-                "offsiteServer": config.offsiteServer,
-                "offsiteUsername": config.offsiteUsername,
-                "parameter1": config.parameter1,
-                "parameter2": config.parameter2,
-                "parameter3": config.parameter3,
-                "parameter4": config.parameter4,
-                "parameter5": config.parameter5,
-                "parameter6": config.parameter6,
-                "dateRun": config.dateRun!,
-                "hiddenID": config.hiddenID,
-            ]
-
-     dict.setValue(ViewControllerReference.shared.snapshot, forKey: "task")
-     dict.setValue(1, forKey: "snapshotnum")
-     ViewControllerReference.shared.syncremote, forKey: "task")
-     */
     init(dict: NSMutableDictionary) {
         guard (dict.value(forKey: "localCatalog") as? String)?.isEmpty == false, (dict.value(forKey: "offsiteCatalog") as? String)?.isEmpty == false else { return }
         guard (dict.value(forKey: "localCatalog") as? String) != "/" || (dict.value(forKey: "offsiteCatalog") as? String) != "/" else { return }
