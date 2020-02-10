540b7505bd14a13262a36bc86793fcc4aeb54753
Thomas Evensen <thomeven@gmail.com>
2020-02-10 20:41:37 +0100

    Update

--- RsyncOSX/Info.swift
+++ RsyncOSX/Info.swift
@@ -16,9 +16,6 @@ struct Infoexecute {
     let info13: String = NSLocalizedString("No rsync in path...", comment: "Execute")
     let info14: String = NSLocalizedString("⌘A to abort or wait...", comment: "Execute")
     let info15: String = NSLocalizedString("Menu app is running...", comment: "Execute")
-    let info16: String = NSLocalizedString("This is a combined task, execute by ⌘R...", comment: "Execute")
-    let info17: String = NSLocalizedString("Only valid for backup, snapshot and combined tasks...", comment: "Execute")
-    let info18: String = NSLocalizedString("No rclone config found...", comment: "Execute")
     let info19: String = NSLocalizedString("New version is available - see About", comment: "Execute")
 
     // Execute
@@ -34,12 +31,6 @@ struct Infoexecute {
             return self.info14
         case 5:
             return self.info15
-        case 6:
-            return self.info16
-        case 7:
-            return self.info17
-        case 8:
-            return self.info18
         case 9:
             return self.info19
         default:
