68a24f0653eab20bcc523dd3f7cc4a5776e9621c
Thomas Evensen <thomeven@gmail.com>
2020-07-15 18:37:00 +0200

    update

--- RsyncOSX/ConvertOneConfig.swift
+++ RsyncOSX/ConvertOneConfig.swift
@@ -14,18 +14,20 @@ struct ConvertOneConfig {
 
     var dict: NSMutableDictionary {
         let row: NSMutableDictionary = [
-            "taskCellID": self.config!.task,
-            "hiddenID": self.config!.hiddenID,
-            "localCatalogCellID": self.config!.localCatalog,
-            "offsiteCatalogCellID": self.config!.offsiteCatalog,
-            "offsiteUsernameID": self.config!.offsiteUsername,
-            "offsiteServerCellID": self.config!.offsiteServer,
-            "backupIDCellID": self.config!.backupID,
-            "runDateCellID": self.config!.dateRun ?? "",
-            "daysID": self.config!.dayssincelastbackup ?? "",
-            "markdays": self.config!.markdays,
-            "snapCellID": self.config!.snapshotnum ?? "",
+            "taskCellID": self.config?.task ?? "",
+            "hiddenID": self.config?.hiddenID ?? "",
+            "localCatalogCellID": self.config?.localCatalog ?? "",
+            "offsiteCatalogCellID": self.config?.offsiteCatalog ?? "",
+            "offsiteUsernameID": self.config?.offsiteUsername ?? "",
+            "offsiteServerCellID": self.config?.offsiteServer ?? "",
+            "backupIDCellID": self.config?.backupID ?? "",
+            "runDateCellID": self.config?.dateRun ?? "",
+            "daysID": self.config?.dayssincelastbackup ?? "",
+            "markdays": self.config?.markdays ?? "",
+            "snapCellID": self.config?.snapshotnum ?? "",
             "selectCellID": 0,
+            "executepretask": self.config?.executepretask ?? 0,
+            "executeposttask": self.config?.executeposttask ?? 0,
         ]
         return row
     }
