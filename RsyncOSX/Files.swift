bf3d5124c19570af69a4a24cd43c00e6b0dc6fcd
Thomas Evensen <thomeven@gmail.com>
2020-07-29 08:40:55 +0200

    Update Files.swift

--- RsyncOSX/Files.swift
+++ RsyncOSX/Files.swift
@@ -56,27 +56,6 @@ extension ErrorMessage {
 }
 
 class Files: NamesandPaths, FileErrors {
-    /*
-     // Function for returning files in path as array of URLs
-      func getFilesURLs() -> [URL]? {
-          var array: [URL]?
-          if let filePath = self.rootpath {
-              let fileManager = FileManager.default
-              var isDir: ObjCBool = false
-              if fileManager.fileExists(atPath: filePath, isDirectory: &isDir) {
-                  guard isDir.boolValue else { return nil }
-              } else { return nil }
-              if let fileURLs = self.getfileURLs(path: filePath) {
-                  array = [URL]()
-                  for i in 0 ..< fileURLs.count where fileURLs[i].isFileURL {
-                      array?.append(fileURLs[i])
-                  }
-                  return array
-              }
-          }
-          return nil
-      }
-      */
     func getcatalogsasURLnames() -> [URL]? {
         if let atpath = self.rootpath {
             do {
@@ -92,28 +71,6 @@ class Files: NamesandPaths, FileErrors {
         return nil
     }
 
-    /*
-     // Function for returning files in path as array of Strings
-     func getFileStrings() -> [String]? {
-         var array: [String]?
-         if let filePath = self.rootpath {
-             let fileManager = FileManager.default
-             var isDir: ObjCBool = false
-             if fileManager.fileExists(atPath: filePath, isDirectory: &isDir) {
-                 guard isDir.boolValue else { return nil }
-             } else { return nil }
-             if let fileURLs = self.getfileURLs(path: filePath) {
-                 array = [String]()
-                 for i in 0 ..< fileURLs.count where fileURLs[i].isFileURL {
-                     array?.append(fileURLs[i].path)
-                 }
-                 return array
-             }
-         }
-         return nil
-     }
-     */
-
     func getfilesasstringnames() -> [String]? {
         if let atpath = self.rootpath {
             do {
@@ -128,24 +85,6 @@ class Files: NamesandPaths, FileErrors {
         }
         return nil
     }
-
-    /*
-     // Function for returning profiles as array of Strings
-     func getDirectorysStrings() -> [String] {
-         var array = [String]()
-         if let filePath = self.rootpath {
-             if let fileURLs = self.getfileURLs(path: filePath) {
-                 for i in 0 ..< fileURLs.count where fileURLs[i].hasDirectoryPath {
-                     let path = fileURLs[i].pathComponents
-                     let i = path.count
-                     array.append(path[i - 1])
-                 }
-                 return array
-             }
-         }
-         return array
-     }
-     */
 
     func getcatalogsasstringnames() -> [String]? {
         if let atpath = self.rootpath {
@@ -175,40 +114,6 @@ class Files: NamesandPaths, FileErrors {
                     self.error(error: error.description, errortype: .profilecreatedirectory)
                 }
             }
-        }
-    }
-
-    /*
-     func createprofilecatalog() {
-         if let docupath = self.docupath {
-             do {
-                 let path = docupath + (self.configpath ?? "")
-                 try Folder(path: path).createSubfolder(named: path)
-             } catch {}
-             if let macserial = self.macserial {
-                 do {
-                     let path = docupath + (self.configpath ?? "") + macserial
-                     try Folder(path: path).createSubfolder(named: path)
-                 } catch {}
-             }
-         }
-     }
-     */
-
-    // Function for getting fileURLs for a given path
-    func getfileURLs(path: String) -> [URL]? {
-        let fileManager = FileManager.default
-        if let filepath = URL(string: path) {
-            do {
-                let files = try fileManager.contentsOfDirectory(at: filepath, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-                return files
-            } catch let e {
-                let error = e as NSError
-                self.error(error: error.description, errortype: .profilecreatedirectory)
-                return nil
-            }
-        } else {
-            return nil
         }
     }
 
