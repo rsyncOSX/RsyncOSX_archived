9d8607fa52d299461aabdc488e21e0b77f00c3a6
Thomas Evensen <thomeven@gmail.com>
2020-08-10 12:03:04 +0200

    update

--- RsyncOSX/ScheduleWriteLoggData.swift
+++ RsyncOSX/ScheduleWriteLoggData.swift
@@ -5,7 +5,7 @@
 //  Created by Thomas Evensen on 19.04.2017.
 //  Copyright © 2017 Thomas Evensen. All rights reserved.
 //
-// swiftlint:disable line_length for_where
+// swiftlint:disable line_length
 
 import Cocoa
 import Foundation
