e6e85cb6c5ae197fda809f5d0ddd49bc20898c34
Thomas Evensen <thomeven@gmail.com>
2020-08-26 20:58:21 +0200

    Update ScheduleSortedAndExpanded.swift

--- RsyncOSX/ScheduleSortedAndExpanded.swift
+++ RsyncOSX/ScheduleSortedAndExpanded.swift
@@ -128,10 +128,12 @@ class ScheduleSortedAndExpand: SetConfigurations, SetSchedules {
         guard (self.sortedschedules?.count ?? 0) > 1 else { return }
         let timestring = Dateandtime()
         self.sortedschedules?[0].setValue(timestring.timestring(seconds: 0), forKey: "delta")
+        self.sortedschedules?[0].setValue(0, forKey: "queuenumber")
         for i in 1 ..< (self.sortedschedules?.count ?? 0) {
             if let t1 = self.sortedschedules?[i - 1].value(forKey: "timetostart") as? Double {
                 if let t2 = self.sortedschedules?[i].value(forKey: "timetostart") as? Double {
                     self.sortedschedules?[i].setValue(timestring.timestring(seconds: t2 - t1), forKey: "delta")
+                    self.sortedschedules?[i].setValue(i, forKey: "queuenumber")
                 }
             }
         }
