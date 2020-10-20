cb0d4aac91f67ebcf964b39b6816965930a44ee8
Thomas Evensen <thomeven@gmail.com>
2020-10-20 07:50:12 +0200

    Update ConfigurationsXCTEST.swift

--- RsyncOSX/ConfigurationsXCTEST.swift
+++ RsyncOSX/ConfigurationsXCTEST.swift
@@ -42,7 +42,7 @@ class ConfigurationsXCTEST: Configurations {
         self.appendconfigurationstomemory(dict: array[array.count - 1])
     }
 
-    override func readconfigurations() {
+    func readconfigurationsJSNON() {
         self.argumentAllConfigurations = [ArgumentsOneConfiguration]()
         // let store: [Configuration]? = PersistentStorageConfiguration(profile: self.profile, allprofiles: true).readconfigurations()
         let store = ReadWriteConfigurationsJSON(profile: self.profile).decodejson
@@ -52,6 +52,31 @@ class ConfigurationsXCTEST: Configurations {
                 self.configurations?.append(transformed)
                 let rsyncArgumentsOneConfig = ArgumentsOneConfiguration(config: transformed)
                 self.argumentAllConfigurations?.append(rsyncArgumentsOneConfig)
+            }
+        }
+        // Then prepare the datasource for use in tableviews as Dictionarys
+        var data = [NSMutableDictionary]()
+        for i in 0 ..< (self.configurations?.count ?? 0) {
+            let task = self.configurations?[i].task
+            if ViewControllerReference.shared.synctasks.contains(task ?? "") {
+                if let config = self.configurations?[i] {
+                    data.append(ConvertOneConfig(config: config).dict)
+                }
+            }
+        }
+        self.configurationsDataSource = data
+    }
+
+    override func readconfigurations() {
+        self.argumentAllConfigurations = [ArgumentsOneConfiguration]()
+        let store: [Configuration]? = PersistentStorageConfiguration(profile: self.profile, allprofiles: true).readconfigurations()
+        for i in 0 ..< (store?.count ?? 0) {
+            if ViewControllerReference.shared.synctasks.contains(store?[i].task ?? "") {
+                if let config = store?[i] {
+                    self.configurations?.append(config)
+                    let rsyncArgumentsOneConfig = ArgumentsOneConfiguration(config: config)
+                    self.argumentAllConfigurations?.append(rsyncArgumentsOneConfig)
+                }
             }
         }
         // Then prepare the datasource for use in tableviews as Dictionarys
