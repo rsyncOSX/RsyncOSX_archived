cff2b47cebade64c23e902b13f38186d324f0f67
Thomas Evensen <thomeven@gmail.com>
2020-11-14 13:01:45 +0100

    Update Dateandtime.swift

--- RsyncOSX/Dateandtime.swift
+++ RsyncOSX/Dateandtime.swift
@@ -19,30 +19,6 @@ struct Dateandtime {
     }
 
     // Calculation of time to a spesific date
-    func timestring(startdate: Date, enddate: Date?) -> String {
-        var result: String?
-        let seconds: Double = self.seconds(startdate: startdate, enddate: enddate)
-        let (hr, minf) = modf(seconds / 3600)
-        let (min, secf) = modf(60 * minf)
-        // hr, min, 60 * secf
-        if hr == 0, min == 0 {
-            result = String(format: "%.0f", 60 * secf) + " " + NSLocalizedString("secs", comment: "datetime")
-        } else if hr == 0, min < 60 {
-            result = String(format: "%.0f", min) + " " + NSLocalizedString("mins", comment: "datetime")
-                + String(format: "%.0f", 60 * secf) + " " + "secs"
-        } else if hr < 25 {
-            result = String(format: "%.0f", hr) + " " + NSLocalizedString("hours", comment: "datetime")
-                + String(format: "%.0f", min) + " " + "mins"
-        } else {
-            result = String(format: "%.0f", hr / 24) + " " + NSLocalizedString(DictionaryStrings.days.rawValue, comment: "datetime")
-        }
-        if secf <= 0 {
-            result = "... working ..."
-        }
-        return result ?? ""
-    }
-
-    // Calculation of time to a spesific date
     func timestring(seconds: Double) -> String {
         var result: String?
         let (hr, minf) = modf(seconds / 3600)
