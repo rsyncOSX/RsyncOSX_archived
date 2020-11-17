72de180c13d23a6a20cca81f73c85922742b82ad
Thomas Evensen <thomeven@gmail.com>
2020-11-17 06:59:30 +0100

    Update AllConfigurations.swift

--- RsyncOSX/AllConfigurations.swift
+++ RsyncOSX/AllConfigurations.swift
@@ -15,10 +15,9 @@ final class AllConfigurations: Sorting {
     private var allprofiles: [String]?
 
     private func readallconfigurations() {
-        guard self.allprofiles != nil else { return }
         var configurations: [Configuration]?
         for i in 0 ..< (self.allprofiles?.count ?? 0) {
-            let profile = self.allprofiles![i]
+            let profile = self.allprofiles?[i]
             if self.allconfigurations == nil {
                 self.allconfigurations = []
             }
@@ -29,39 +28,41 @@ final class AllConfigurations: Sorting {
             }
             guard configurations != nil else { return }
             for j in 0 ..< (configurations?.count ?? 0) {
-                configurations![j].profile = profile
-                self.allconfigurations!.append(configurations![j])
+                configurations?[j].profile = profile
+                if let config = configurations?[j] {
+                    self.allconfigurations?.append(config)
+                }
             }
         }
     }
 
     private func setConfigurationsDataSourcecountBackupSnapshot() {
         guard self.allconfigurations != nil else { return }
-        var configurations: [Configuration] = self.allconfigurations!.filter {
+        var configurations = self.allconfigurations?.filter {
             ViewControllerReference.shared.synctasks.contains($0.task)
         }
         var data = [NSMutableDictionary]()
-        for i in 0 ..< configurations.count {
-            if configurations[i].offsiteServer.isEmpty == true {
-                configurations[i].offsiteServer = "localhost"
+        for i in 0 ..< (configurations?.count ?? 0) {
+            if configurations?[i].offsiteServer.isEmpty == true {
+                configurations?[i].offsiteServer = "localhost"
             }
             var date: String = ""
-            let stringdate = configurations[i].dateRun ?? ""
+            let stringdate = configurations?[i].dateRun ?? ""
             if stringdate.isEmpty == false {
                 date = stringdate.en_us_date_from_string().localized_string_from_date()
             }
             let row: NSMutableDictionary = [
-                DictionaryStrings.profile.rawValue: configurations[i].profile ?? "",
-                DictionaryStrings.task.rawValue: configurations[i].task,
-                DictionaryStrings.hiddenID.rawValue: configurations[i].hiddenID,
-                DictionaryStrings.localCatalog.rawValue: configurations[i].localCatalog,
-                DictionaryStrings.offsiteCatalog.rawValue: configurations[i].offsiteCatalog,
-                DictionaryStrings.offsiteServer.rawValue: configurations[i].offsiteServer,
-                DictionaryStrings.offsiteUsername.rawValue: configurations[i].offsiteUsername,
-                DictionaryStrings.backupID.rawValue: configurations[i].backupID,
+                DictionaryStrings.profile.rawValue: configurations?[i].profile ?? "",
+                DictionaryStrings.task.rawValue: configurations?[i].task ?? "",
+                DictionaryStrings.hiddenID.rawValue: configurations?[i].hiddenID ?? -1,
+                DictionaryStrings.localCatalog.rawValue: configurations?[i].localCatalog ?? "",
+                DictionaryStrings.offsiteCatalog.rawValue: configurations?[i].offsiteCatalog ?? "",
+                DictionaryStrings.offsiteServer.rawValue: configurations?[i].offsiteServer ?? "",
+                DictionaryStrings.offsiteUsername.rawValue: configurations?[i].offsiteUsername ?? "",
+                DictionaryStrings.backupID.rawValue: configurations?[i].backupID ?? "",
                 DictionaryStrings.dateExecuted.rawValue: date,
-                DictionaryStrings.daysID.rawValue: configurations[i].dayssincelastbackup ?? "",
-                DictionaryStrings.markdays.rawValue: configurations[i].markdays,
+                DictionaryStrings.daysID.rawValue: configurations?[i].dayssincelastbackup ?? "",
+                DictionaryStrings.markdays.rawValue: configurations?[i].markdays ?? false,
                 DictionaryStrings.selectCellID.rawValue: 0,
             ]
             data.append(row)
