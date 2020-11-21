70eb49f3cc3bdcd30c03faef80f55a4a5a636481
Thomas Evensen <thomeven@gmail.com>
2020-11-21 11:40:12 +0100

    Update SchedulesData.swift

--- RsyncOSX/SchedulesData.swift
+++ RsyncOSX/SchedulesData.swift
@@ -5,6 +5,7 @@
 //  Created by Thomas Evensen on 15/11/2020.
 //  Copyright © 2020 Thomas Evensen. All rights reserved.
 //
+// swiftlint:disable line_length
 
 import Foundation
 
@@ -12,32 +13,35 @@ final class SchedulesData {
     var schedules: [ConfigurationSchedule]?
     var profile: String?
     var validhiddenID: Set<Int>?
+    var persistentstorage: PersistentStorage?
 
     func readschedulesplist() {
-        let schedulesfromstore = PersistentStorageSchedulingPLIST(profile: self.profile).schedulesasdictionary
-        var schedule: ConfigurationSchedule?
-        for i in 0 ..< (schedulesfromstore?.count ?? 0) {
-            if let dict = schedulesfromstore?[i], let validhiddenID = self.validhiddenID {
-                if let hiddenID = dict.value(forKey: DictionaryStrings.hiddenID.rawValue) as? Int {
-                    if validhiddenID.contains(hiddenID) {
-                        if let log = dict.value(forKey: DictionaryStrings.executed.rawValue) {
-                            schedule = ConfigurationSchedule(dictionary: dict, log: log as? NSArray, includelog: true)
-                        } else {
-                            schedule = ConfigurationSchedule(dictionary: dict, log: nil, includelog: true)
-                        }
-                        if let schedule = schedule {
-                            self.schedules?.append(schedule)
+        if let schedulesfromstore = self.persistentstorage?.schedulePLIST?.schedulesasdictionary {
+            var schedule: ConfigurationSchedule?
+            for i in 0 ..< schedulesfromstore.count {
+                if let validhiddenID = self.validhiddenID {
+                    let dict = schedulesfromstore[i]
+                    if let hiddenID = dict.value(forKey: DictionaryStrings.hiddenID.rawValue) as? Int {
+                        if validhiddenID.contains(hiddenID) {
+                            if let log = dict.value(forKey: DictionaryStrings.executed.rawValue) {
+                                schedule = ConfigurationSchedule(dictionary: dict, log: log as? NSArray, includelog: true)
+                            } else {
+                                schedule = ConfigurationSchedule(dictionary: dict, log: nil, includelog: true)
+                            }
+                            if let schedule = schedule {
+                                self.schedules?.append(schedule)
+                            }
                         }
                     }
                 }
             }
-        }
-        // Sorting schedule after hiddenID
-        self.schedules?.sort { (schedule1, schedule2) -> Bool in
-            if schedule1.hiddenID > schedule2.hiddenID {
-                return false
-            } else {
-                return true
+            // Sorting schedule after hiddenID
+            self.schedules?.sort { (schedule1, schedule2) -> Bool in
+                if schedule1.hiddenID > schedule2.hiddenID {
+                    return false
+                } else {
+                    return true
+                }
             }
         }
     }
@@ -45,24 +49,24 @@ final class SchedulesData {
     // Function for reading all jobs for schedule and all history of past executions.
     // Schedules are stored in self.schedules. Schedules are sorted after hiddenID.
     func readschedulesjson() {
-        let store = PersistentStorageSchedulingJSON(profile: self.profile).decodedjson
-        // var data = [ConfigurationSchedule]()
-        let transform = TransformSchedulefromJSON()
-        for i in 0 ..< (store?.count ?? 0) {
-            if let scheduleitem = (store?[i] as? DecodeSchedule), let validhiddenID = self.validhiddenID {
-                var transformed = transform.transform(object: scheduleitem)
-                transformed.profilename = self.profile
-                if validhiddenID.contains(transformed.hiddenID) {
-                    self.schedules?.append(transformed)
+        if let schedulesfromstore = self.persistentstorage?.scheduleJSON?.decodedjson {
+            let transform = TransformSchedulefromJSON()
+            for i in 0 ..< schedulesfromstore.count {
+                if let scheduleitem = (schedulesfromstore[i] as? DecodeSchedule), let validhiddenID = self.validhiddenID {
+                    var transformed = transform.transform(object: scheduleitem)
+                    transformed.profilename = self.profile
+                    if validhiddenID.contains(transformed.hiddenID) {
+                        self.schedules?.append(transformed)
+                    }
                 }
             }
-        }
-        // Sorting schedule after hiddenID
-        self.schedules?.sort { (schedule1, schedule2) -> Bool in
-            if schedule1.hiddenID > schedule2.hiddenID {
-                return false
-            } else {
-                return true
+            // Sorting schedule after hiddenID
+            self.schedules?.sort { (schedule1, schedule2) -> Bool in
+                if schedule1.hiddenID > schedule2.hiddenID {
+                    return false
+                } else {
+                    return true
+                }
             }
         }
     }
@@ -72,11 +76,9 @@ final class SchedulesData {
         self.schedules = nil
         self.validhiddenID = validhiddenID
         self.schedules = [ConfigurationSchedule]()
-        if ViewControllerReference.shared.json {
-            self.readschedulesjson()
-        } else {
-            self.readschedulesplist()
-        }
+        self.persistentstorage = PersistentStorage(profile: self.profile, whattoreadorwrite: .schedule)
+        self.readschedulesjson()
+        self.readschedulesplist()
         if ViewControllerReference.shared.checkinput {
             self.schedules = Reorgschedule().mergerecords(data: self.schedules)
         }
