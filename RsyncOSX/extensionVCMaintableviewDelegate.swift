60ea492776d987071ecf5dc7717ae82bf04dc26d
Thomas Evensen <thomeven@gmail.com>
2020-11-27 08:33:05 +0100

    update

--- RsyncOSX/extensionVCMaintableviewDelegate.swift
+++ RsyncOSX/extensionVCMaintableviewDelegate.swift
@@ -32,83 +32,84 @@ extension ViewControllerMain: NSTableViewDataSource {
 }
 
 extension ViewControllerMain: NSTableViewDelegate, Attributedestring {
-    // TableView delegates
-    func tableView(_: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        if row > (self.configurations?.configurationsDataSource?.count ?? 0) - 1 { return nil }
-        if let object = self.configurations?.getConfigurationsDataSource()?[row],
-           let hiddenID: Int = self.configurations?.getConfigurations()?[row].hiddenID,
-           let markdays = self.configurations?.getConfigurations()?[row].markdays,
-           let tableColumn = tableColumn
-        {
-            let celltext = object[tableColumn.identifier] as? String
-            if tableColumn.identifier.rawValue == DictionaryStrings.daysID.rawValue {
-                if markdays {
-                    return self.attributedstring(str: celltext!, color: NSColor.red, align: .right)
-                } else {
-                    return object[tableColumn.identifier] as? String
-                }
-            } else if tableColumn.identifier.rawValue == DictionaryStrings.offsiteServerCellID.rawValue,
-                      ((object[tableColumn.identifier] as? String)?.isEmpty) == true
-            {
-                return "localhost"
-            } else if tableColumn.identifier.rawValue == "schedCellID" {
-                if let obj = self.schedulesortedandexpanded {
-                    if obj.numberoftasks(hiddenID).0 > 0 {
-                        if obj.numberoftasks(hiddenID).1 > 3600 {
-                            return #imageLiteral(resourceName: "yellow")
-                        } else {
-                            return #imageLiteral(resourceName: "green")
-                        }
-                    }
-                }
-            } else if tableColumn.identifier.rawValue == "statCellID" {
-                if row == self.index {
-                    if self.singletask == nil {
-                        return #imageLiteral(resourceName: "yellow")
-                    } else {
-                        return #imageLiteral(resourceName: "green")
-                    }
-                }
-            } else if tableColumn.identifier.rawValue == DictionaryStrings.snapCellID.rawValue {
-                let snap = object.value(forKey: DictionaryStrings.snapCellID.rawValue) as? Int ?? -1
-                if snap > 0 {
-                    return String(snap - 1)
-                } else {
-                    return ""
-                }
-            } else if tableColumn.identifier.rawValue == DictionaryStrings.runDateCellID.rawValue {
-                let stringdate: String = object[tableColumn.identifier] as? String ?? ""
-                if stringdate.isEmpty {
-                    return ""
-                } else {
-                    return stringdate.en_us_date_from_string().localized_string_from_date()
-                }
-            } else if tableColumn.identifier.rawValue == "Shell" {
-                let pre = object.value(forKey: DictionaryStrings.executepretask.rawValue) as? Int ?? 0
-                let post = object.value(forKey: DictionaryStrings.executeposttask.rawValue) as? Int ?? 0
-                if pre == 1 || post == 1 {
-                    return 1
-                } else {
-                    return 0
-                }
-            } else {
-                if self.configurations?.tcpconnections?.connectionscheckcompleted ?? false == true {
-                    if (self.configurations?.tcpconnections?.gettestAllremoteserverConnections()?[row]) ?? false,
-                       tableColumn.identifier.rawValue == DictionaryStrings.offsiteServerCellID.rawValue
-                    {
-                        return self.attributedstring(str: celltext ?? "", color: NSColor.red, align: .left)
-                    } else {
-                        return object[tableColumn.identifier] as? String
-                    }
-                } else {
-                    return object[tableColumn.identifier] as? String
-                }
-            }
-            return nil
-        }
-        return nil
-    }
-
+    /*
+     // TableView delegates
+     func tableView(_: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+         if row > (self.configurations?.configurationsDataSource?.count ?? 0) - 1 { return nil }
+         if let object = self.configurations?.getConfigurationsDataSource()?[row],
+            let hiddenID: Int = self.configurations?.getConfigurations()?[row].hiddenID,
+            let markdays = self.configurations?.getConfigurations()?[row].markdays,
+            let tableColumn = tableColumn
+         {
+             let celltext = object[tableColumn.identifier] as? String
+             if tableColumn.identifier.rawValue == DictionaryStrings.daysID.rawValue {
+                 if markdays {
+                     return self.attributedstring(str: celltext!, color: NSColor.red, align: .right)
+                 } else {
+                     return object[tableColumn.identifier] as? String
+                 }
+             } else if tableColumn.identifier.rawValue == DictionaryStrings.offsiteServerCellID.rawValue,
+                       ((object[tableColumn.identifier] as? String)?.isEmpty) == true
+             {
+                 return "localhost"
+             } else if tableColumn.identifier.rawValue == "schedCellID" {
+                 if let obj = self.schedulesortedandexpanded {
+                     if obj.numberoftasks(hiddenID).0 > 0 {
+                         if obj.numberoftasks(hiddenID).1 > 3600 {
+                             return #imageLiteral(resourceName: "yellow")
+                         } else {
+                             return #imageLiteral(resourceName: "green")
+                         }
+                     }
+                 }
+             } else if tableColumn.identifier.rawValue == "statCellID" {
+                 if row == self.index {
+                     if self.singletask == nil {
+                         return #imageLiteral(resourceName: "yellow")
+                     } else {
+                         return #imageLiteral(resourceName: "green")
+                     }
+                 }
+             } else if tableColumn.identifier.rawValue == DictionaryStrings.snapCellID.rawValue {
+                 let snap = object.value(forKey: DictionaryStrings.snapCellID.rawValue) as? Int ?? -1
+                 if snap > 0 {
+                     return String(snap - 1)
+                 } else {
+                     return ""
+                 }
+             } else if tableColumn.identifier.rawValue == DictionaryStrings.runDateCellID.rawValue {
+                 let stringdate: String = object[tableColumn.identifier] as? String ?? ""
+                 if stringdate.isEmpty {
+                     return ""
+                 } else {
+                     return stringdate.en_us_date_from_string().localized_string_from_date()
+                 }
+             } else if tableColumn.identifier.rawValue == "Shell" {
+                 let pre = object.value(forKey: DictionaryStrings.executepretask.rawValue) as? Int ?? 0
+                 let post = object.value(forKey: DictionaryStrings.executeposttask.rawValue) as? Int ?? 0
+                 if pre == 1 || post == 1 {
+                     return 1
+                 } else {
+                     return 0
+                 }
+             } else {
+                 if self.configurations?.tcpconnections?.connectionscheckcompleted ?? false == true {
+                     if (self.configurations?.tcpconnections?.gettestAllremoteserverConnections()?[row]) ?? false,
+                        tableColumn.identifier.rawValue == DictionaryStrings.offsiteServerCellID.rawValue
+                     {
+                         return self.attributedstring(str: celltext ?? "", color: NSColor.red, align: .left)
+                     } else {
+                         return object[tableColumn.identifier] as? String
+                     }
+                 } else {
+                     return object[tableColumn.identifier] as? String
+                 }
+             }
+             return nil
+         }
+         return nil
+     }
+     */
     // when row is selected
     // setting which table row is selected, force new estimation
     func tableViewSelectionDidChange(_ notification: Notification) {
@@ -129,5 +130,63 @@ extension ViewControllerMain: NSTableViewDelegate, Attributedestring {
         self.reset()
         self.showrsynccommandmainview()
         self.reloadtabledata()
+    }
+
+    func tableView(_: NSTableView, rowActionsForRow _: Int, edge: NSTableView.RowActionEdge) -> [NSTableViewRowAction] {
+        if edge == .leading {
+            let printAction = NSTableViewRowAction(style: .regular, title: "Print") { _, _ in
+                print("Now printing...")
+            }
+            printAction.backgroundColor = NSColor.gray
+            return [printAction]
+
+        } else {
+            let deleteAction = NSTableViewRowAction(style: .destructive, title: "Delete") { _, _ in
+                // self.viewModel.removePurchase(atIndex: row)
+                // self.tableView.reloadData()
+            }
+
+            return [deleteAction]
+        }
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard self.configurations != nil else { return nil }
+        if row > (self.configurations?.configurationsDataSource?.count ?? 0) - 1 { return nil }
+        if let object: NSDictionary = self.configurations?.getConfigurationsDataSource()?[row],
+           let markdays: Bool = self.configurations?.getConfigurations()?[row].markdays,
+           let tableColumn = tableColumn
+        {
+            let cellIdentifier: String = tableColumn.identifier.rawValue
+            print(cellIdentifier)
+            switch cellIdentifier {
+            case "taskCellID":
+                if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: self) as? NSTableCellView {
+                    cell.textField?.stringValue = object.value(forKey: cellIdentifier) as? String ?? ""
+                    if row == self.index {
+                        if self.singletask == nil {
+                            cell.imageView?.image = NSImage(#imageLiteral(resourceName: "yellow"))
+                        } else {
+                            cell.imageView?.image = NSImage(#imageLiteral(resourceName: "green"))
+                        }
+                    }
+                    return cell
+                }
+            case "offsiteServerCellID":
+                if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: self) as? NSTableCellView {
+                    cell.textField?.stringValue = object.value(forKey: cellIdentifier) as? String ?? ""
+                    if cell.textField?.stringValue.isEmpty ?? true {
+                        cell.textField?.stringValue = "localhost"
+                    }
+                    return cell
+                }
+            default:
+                if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: self) as? NSTableCellView {
+                    cell.textField?.stringValue = object.value(forKey: cellIdentifier) as? String ?? ""
+                    return cell
+                }
+            }
+        }
+        return nil
     }
 }
