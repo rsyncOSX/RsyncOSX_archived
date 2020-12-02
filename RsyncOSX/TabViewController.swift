f14f3de0c3f570358188f6dd6dbb0bcb2b9b85a5
Thomas Evensen <thomeven@gmail.com>
2020-12-02 07:45:10 +0100

    update

--- RsyncOSX/TabViewController.swift
+++ RsyncOSX/TabViewController.swift
@@ -9,21 +9,15 @@
 import Cocoa
 import Foundation
 
-enum Tabintems: String {
-    case Synchronize
-    case Add
-    case Schedule
-    case Snapshots
-    case Restore
-    case Logs
-    case Ssh
-}
-
 class TabViewController: NSTabViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func setsize() {
         self.view.frame.size.width = 995 + 12
         self.view.frame.size.height = 350 + 12
         self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setsize()
     }
 }
