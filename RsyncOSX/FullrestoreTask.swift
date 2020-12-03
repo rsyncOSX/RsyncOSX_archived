7b2be7070aec8ca39e7d2202c487b37f57d63743
Thomas Evensen <thomeven@gmail.com>
2020-12-03 09:44:36 +0100

    update

--- RsyncOSX/FullrestoreTask.swift
+++ RsyncOSX/FullrestoreTask.swift
@@ -10,14 +10,19 @@
 import Foundation
 
 final class FullrestoreTask: SetConfigurations {
+    private var config: Configuration?
     var arguments: [String]?
+    var dryrun: Bool = true
     weak var sendprocess: SendOutputProcessreference?
     var process: RsyncProcessCmdClosure?
     var outputprocess: OutputProcess?
 
-    init(index: Int, dryrun: Bool, processtermination: @escaping () -> Void, filehandler: @escaping () -> Void) {
-        self.sendprocess = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
-        if dryrun {
+    // Process termination and filehandler closures
+    var processtermination: () -> Void
+    var filehandler: () -> Void
+
+    func executerestore(index: Int) {
+        if self.dryrun {
             self.arguments = self.configurations?.arguments4tmprestore(index: index, argtype: .argdryRun)
             let lastindex = (self.arguments?.count ?? 0) - 1
             guard lastindex > -1 else { return }
@@ -32,7 +37,14 @@ final class FullrestoreTask: SetConfigurations {
             self.process = RsyncProcessCmdClosure(arguments: arguments, config: nil, processtermination: processtermination, filehandler: filehandler)
             self.outputprocess = OutputProcessRsync()
             self.sendprocess?.sendoutputprocessreference(outputprocess: self.outputprocess)
-            self.process?.executeProcess(outputprocess: outputprocess)
+            self.process?.executeProcess(outputprocess: self.outputprocess)
         }
+    }
+
+    init(dryrun: Bool, processtermination: @escaping () -> Void, filehandler: @escaping () -> Void) {
+        self.sendprocess = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
+        self.processtermination = processtermination
+        self.filehandler = filehandler
+        self.dryrun = dryrun
     }
 }
