8622511e43fda7095acd4884efaaf0cb96d64400
Thomas Evensen <thomeven@gmail.com>
2020-12-08 19:40:05 +0100

    update

--- RsyncOSX/AllConfigurations.swift
+++ RsyncOSX/AllConfigurations.swift
@@ -10,8 +10,7 @@
 import Foundation
 
 final class AllConfigurations: Sorting {
-    private var allconfigurations: [Configuration]?
-    var allconfigurationsasdictionary: [NSMutableDictionary]?
+    var allconfigurations: [Configuration]?
     private var allprofiles: [String]?
 
     private func readallconfigurations() {
@@ -36,55 +35,20 @@ final class AllConfigurations: Sorting {
         }
     }
 
-    private func setConfigurationsDataSourcecountBackupSnapshot() {
-        guard self.allconfigurations != nil else { return }
-        var configurations = self.allconfigurations?.filter {
-            ViewControllerReference.shared.synctasks.contains($0.task)
-        }
-        var data = [NSMutableDictionary]()
-        for i in 0 ..< (configurations?.count ?? 0) {
-            if configurations?[i].offsiteServer.isEmpty == true {
-                configurations?[i].offsiteServer = DictionaryStrings.localhost.rawValue
-            }
-            var date: String = ""
-            let stringdate = configurations?[i].dateRun ?? ""
-            if stringdate.isEmpty == false {
-                date = stringdate.en_us_date_from_string().localized_string_from_date()
-            }
-            let row: NSMutableDictionary = [
-                DictionaryStrings.profile.rawValue: configurations?[i].profile ?? "",
-                DictionaryStrings.task.rawValue: configurations?[i].task ?? "",
-                DictionaryStrings.hiddenID.rawValue: configurations?[i].hiddenID ?? -1,
-                DictionaryStrings.localCatalog.rawValue: configurations?[i].localCatalog ?? "",
-                DictionaryStrings.offsiteCatalog.rawValue: configurations?[i].offsiteCatalog ?? "",
-                DictionaryStrings.offsiteServer.rawValue: configurations?[i].offsiteServer ?? "",
-                DictionaryStrings.offsiteUsername.rawValue: configurations?[i].offsiteUsername ?? "",
-                DictionaryStrings.backupID.rawValue: configurations?[i].backupID ?? "",
-                DictionaryStrings.dateExecuted.rawValue: date,
-                DictionaryStrings.daysID.rawValue: configurations?[i].dayssincelastbackup ?? "",
-                DictionaryStrings.markdays.rawValue: configurations?[i].markdays ?? false,
-                DictionaryStrings.selectCellID.rawValue: 0,
-            ]
-            data.append(row)
-        }
-        self.allconfigurationsasdictionary = data
-    }
-
-    // Function for filter
-    func filter(search: String?, filterby: Sortandfilter?) {
-        guard search != nil, self.allconfigurationsasdictionary != nil, filterby != nil else { return }
-        globalDefaultQueue.async { () -> Void in
-            let valueforkey = self.filterbystring(filterby: filterby!)
-            let filtereddata = self.allconfigurationsasdictionary?.filter {
-                ($0.value(forKey: valueforkey) as? String)?.contains(search ?? "") ?? false
-            }
-            self.allconfigurationsasdictionary = filtereddata
+    func sorttest(data: [Configuration], type: Int) -> [Configuration]? {
+        switch type {
+        case 0:
+            return data.sorted(by: \.backupID, using: >)
+        case 1:
+            return data.sorted(by: \.offsiteCatalog, using: >)
+        default:
+            return data.sorted(by: \.dateRun!, using: >)
         }
     }
 
     init() {
         self.allprofiles = AllProfilenames().allprofiles
         self.readallconfigurations()
-        self.setConfigurationsDataSourcecountBackupSnapshot()
+        // self.setConfigurationsDataSourcecountBackupSnapshot()
     }
 }
--- RsyncOSX/ViewControllerExtensions.swift
+++ RsyncOSX/ViewControllerExtensions.swift
@@ -414,3 +414,14 @@ extension Sorting {
         }
     }
 }
+
+extension Sequence {
+    func sorted<T: Comparable>(
+        by keyPath: KeyPath<Element, T>,
+        using comparator: (T, T) -> Bool = (<)
+    ) -> [Element] {
+        sorted { a, b in
+            comparator(a[keyPath: keyPath], b[keyPath: keyPath])
+        }
+    }
+}
