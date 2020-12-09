d869df1bdaeeecdcc4519c519f5154b9385c3aa1
Thomas Evensen <thomeven@gmail.com>
2020-12-09 08:46:59 +0100

    update

---
+++ RsyncOSX/ConfigurationsAsDictionarys.swift
@@ -0,0 +1,40 @@
+//
+//  ConfigurationsAsDictionarys.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 09/12/2020.
+//  Copyright © 2020 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+
+struct ConfigurationsAsDictionarys: SetConfigurations {
+    
+    var quickbackuplist: [Int]?
+    
+    // Function for getting all Configurations
+    func getConfigurationsDataSourceSynchronize() -> [NSMutableDictionary]? {
+        guard self.configurations != nil else { return nil }
+        var configurations = self.configurations?.configurations?.filter {
+            ViewControllerReference.shared.synctasks.contains($0.task)
+        }
+        var data = [NSMutableDictionary]()
+        for i in 0 ..< (configurations?.count ?? 0) {
+            if configurations?[i].offsiteServer.isEmpty == true {
+                configurations?[i].offsiteServer = DictionaryStrings.localhost.rawValue
+            }
+            if let config = self.configurations?.configurations?[i] {
+                let row: NSMutableDictionary = ConvertOneConfig(config: config).dict
+
+                if self.quickbackuplist != nil {
+                    let quickbackup = self.quickbackuplist?.filter { $0 == config.hiddenID }
+                    if (quickbackup?.count ?? 0) > 0 {
+                        row.setValue(1, forKey: DictionaryStrings.selectCellID.rawValue)
+                    }
+                }
+                data.append(row)
+            }
+        }
+        return data
+    }
+}
