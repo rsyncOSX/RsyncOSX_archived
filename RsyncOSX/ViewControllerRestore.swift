5c1b5f7af40261127420bc86f90ca8f055d458d1
Thomas Evensen <22427340+rsyncOSX@users.noreply.github.com>
2021-12-18 20:36:42 +0100

    Update ViewControllerRestore.swift

--- RsyncOSX/ViewControllerRestore.swift
+++ RsyncOSX/ViewControllerRestore.swift
@@ -5,7 +5,7 @@
 //  Created by Thomas Evensen on 12/09/2016.
 //  Copyright © 2016 Thomas Evensen. All rights reserved.
 //
-//  swiftlint:disable line_length type_body_length
+//  swiftlint:disable line_length
 
 import Cocoa
 import Foundation
